066e9d71b2864c825157b40eac4bf903131ac6cb
Шпрыгин Ярослав <iarik2000.00@mail.ru>
2017-02-01 10:38:16 +0300

    Add files via upload

---
+++ XIX/jscript.js
@@ -0,0 +1,69 @@
+function show_events() {
+
+    var events_elem = document.getElementById("events");
+    var science_elem = document.getElementById("science");
+    var leature_elem = document.getElementById("leature");
+    var railroad_elem = document.getElementById("railroad");
+    var people_elem = document.getElementById("people");
+    events_elem.style.display = 'block';
+    science_elem.style.display = 'none';
+    leature_elem.style.display = 'none';
+    railroad_elem.style.display = 'none';
+    people_elem.style.display = 'none';
+}
+function show_science() {
+
+    var events_elem = document.getElementById("events");
+    var science_elem = document.getElementById("science");
+    var leature_elem = document.getElementById("leature");
+    var railroad_elem = document.getElementById("railroad");
+    var people_elem = document.getElementById("people");
+    events_elem.style.display = 'none';
+    science_elem.style.display = 'block';
+    leature_elem.style.display = 'none';
+    railroad_elem.style.display = 'none';
+    people_elem.style.display = 'none';
+
+}
+function show_leature() {
+
+    var events_elem = document.getElementById("events");
+    var science_elem = document.getElementById("science");
+    var leature_elem = document.getElementById("leature");
+    var railroad_elem = document.getElementById("railroad");
+    var people_elem = document.getElementById("people");
+    events_elem.style.display = 'none';
+    science_elem.style.display = 'none';
+    leature_elem.style.display = 'block';
+    railroad_elem.style.display = 'none';
+    people_elem.style.display = 'none';
+
+}
+function show_railroad() {
+
+    var events_elem = document.getElementById("events");
+    var science_elem = document.getElementById("science");
+    var leature_elem = document.getElementById("leature");
+    var railroad_elem = document.getElementById("railroad");
+    var people_elem = document.getElementById("people");
+    events_elem.style.display = 'none';
+    science_elem.style.display = 'none';
+    leature_elem.style.display = 'none';
+    railroad_elem.style.display = 'block';
+    people_elem.style.display = 'none';
+
+}
+function show_people() {
+
+    var events_elem = document.getElementById("events");
+    var science_elem = document.getElementById("science");
+    var leature_elem = document.getElementById("leature");
+    var railroad_elem = document.getElementById("railroad");
+    var people_elem = document.getElementById("people");
+    events_elem.style.display = 'none';
+    science_elem.style.display = 'none';
+    leature_elem.style.display = 'none';
+    railroad_elem.style.display = 'none';
+    people_elem.style.display = 'block';
+
+}
